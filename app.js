9c009c91adb691968ff9bdde4dfda6c797567f69
Kris Henderson <krishenderson@gmail.com>
2017-03-03 19:05:52 +0000

    add mongolab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes    = require("./routes/comments")
 var campgroundRoutes = require("./routes/campgrounds")
 var indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://kris:307608kH@ds113660.mlab.com:13660/yelpcamp123")
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
